74dde5a7e44b48fb21501187556fd856ecaf401d
Dilbara Asanalieva <dilbara2025@gmail.com>
2022-04-21 13:19:28 +0600

    user service

--- src/main/java/peaksoft/Main.java
+++ src/main/java/peaksoft/Main.java
@@ -1,7 +1,42 @@
 package peaksoft;
 
+import peaksoft.model.User;
+import peaksoft.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+
+        UserServiceImpl userService = new UserServiceImpl();
+//        userService.createUsersTable();
+//
+//
+//        userService.saveUser("Dilbara","Asanalieva",(byte)19);
+//        userService.saveUser("Chynara","Mamtalieva",(byte)34);
+//        userService.saveUser("Aichurok","Turganbaeva",(byte)33);
+//        userService.removeUserById(2);
+//        System.out.println(userService.getAllUsers());
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
+        System.out.println(userService.existsByFirstName("Dilbara"));
     }
 }
+
+
+
+//
+//    private final Connection connection;
+//
+//    public Main() throws SQLException {
+//        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/project_db",
+//                "postgres",
+//                "postgresql22");
+//    }
+//    public Connection getConnection(){
+//        return connection;
+//    }
+//}
--- src/main/java/peaksoft/service/UserServiceImpl.java
+++ src/main/java/peaksoft/service/UserServiceImpl.java
@@ -1,38 +1,125 @@
 package peaksoft.service;
 
+import peaksoft.Main;
 import peaksoft.model.User;
+import peaksoft.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    public void createUsersTable() {
+    private final Connection connection;
 
+    public UserServiceImpl() throws SQLException {
+        connection = new Util().getConnection();
     }
+
+    public void createUsersTable() {
+        String query = """
+                create table if not exists users(
+                id serial primary key,
+                name varchar(50) not null,
+                last_name varchar(50) not null,
+                age smallint not null
+                );
+                """;
+
+        try (Statement statement = connection.createStatement();) {
+            statement.execute(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public void dropUsersTable() {
+        String query = "drop table users;";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+//            System.out.println("You have successfully dropped students table!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void saveUser(String name, String last_name, byte age) {
+        String query = """
+                insert into users(name,last_name,age) values (?,?,?)
+                """;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, last_name);
+            preparedStatement.setByte(3, age);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        String query = "delete from users where id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String query = "select * from users;";
+        List<User> allUsers = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte(4));
+                allUsers.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return allUsers;
     }
 
     public void cleanUsersTable() {
+        String query = "truncate table users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+            System.out.println("You have successfully deleted all users ");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean existsByFirstName(String firstName) {
         // eger databasede parametrine kelgen firstnamege okshosh adam bar bolso
         // anda true kaitarsyn
         // jok bolso anda false kaitarsyn.
+//        String query = "SELECT CASE when count( users.name) > 0 then true else false end from users where users.name like ?";
+
+        String query = "select case when count(*)>0  then true else false end from users where name like ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, firstName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getBoolean(1);
+            }
+            resultSet.close();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return false;
     }
 }
+
+
+
